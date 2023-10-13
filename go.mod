d10ee110f3cc06bc95f424fbd4a9195c42529df5
rajibmitra <rajibmitter@gmail.com>
2023-10-13 21:12:11 +0530

    changed  main and mod file

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/rajibmitra/grep-crawler
 
 go 1.20
-
-require golang.org/x/net v0.17.0 // indirect
--- main.go
+++ main.go
@@ -2,65 +2,48 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"golang.org/x/net/html"
+	"os"
+	"os/exec"
+	"sync"
 )
 
 func main() {
-	// URL to start crawling from
-	startURL := "https://example.com"
-
-	// Initialize a map to keep track of visited URLs
-	visited := make(map[string]bool)
-
-	// Create a channel to send URLs to be crawled
-	queue := make(chan string)
-
-	// Start the crawling process by visiting the initial URL
-	go func() {
-		queue <- startURL
-	}()
-
-	for url := range queue {
-		// Mark the URL as visited
-		visited[url] = true
-
-		// Fetch the web page
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println("Error fetching URL:", err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		// Parse the HTML content
-		tokenizer := html.NewTokenizer(resp.Body)
-		for {
-			tokenType := tokenizer.Next()
-			switch tokenType {
-			case html.ErrorToken:
-				return
-			case html.StartTagToken, html.SelfClosingTagToken:
-				token := tokenizer.Token()
-				if token.Data == "a" {
-					for _, attr := range token.Attr {
-						if attr.Key == "href" {
-							// Extract the link URL
-							linkURL := attr.Val
-
-							// Ensure the link URL is not visited already
-							if !visited[linkURL] {
-								fmt.Println(linkURL)
-								// Add the link URL to the queue for further crawling
-								go func() {
-									queue <- linkURL
-								}()
-							}
-						}
-					}
-				}
-			}
-		}
+	// Ensure the user provides the pattern and at least one directory argument
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: grep-tool <pattern> <directory1> [directory2...]")
+		os.Exit(1)
 	}
+
+	// Extract the pattern and directory arguments
+	pattern := os.Args[1]
+	directories := os.Args[2:]
+
+	// Create a WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Iterate over the provided directories and search concurrently
+	for _, dir := range directories {
+		wg.Add(1)
+		go func(directory string) {
+			defer wg.Done()
+
+			fmt.Printf("Searching for '%s' in '%s' (excluding .git directory):\n", pattern, directory)
+
+			// Run the grep command with the pattern, directory, and exclude .git directory
+			cmd := exec.Command("grep", "-rn", "--exclude-dir=.git", pattern, directory)
+
+			// Capture the output of grep
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Printf("Error running grep: %v\n", err)
+				return
+			}
+
+			// Print the grep results
+			fmt.Printf("%s\n", output)
+		}(dir)
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
 }
